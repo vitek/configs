78067c89e7d480d0955a4c753f7500735925ecd6
Vitja Makarov <vitja.makarov@gmail.com>
2011-05-08 09:10:08 +0400

    Remove mouse wheel bindings

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -60,10 +60,10 @@ end
 -- }}}
 
 -- {{{ Mouse bindings
-root.buttons(awful.util.table.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
+-- root.buttons(awful.util.table.join(
+--     awful.button({ }, 4, awful.tag.viewnext),
+--     awful.button({ }, 5, awful.tag.viewprev)
+-- ))
 -- }}}
 
 
