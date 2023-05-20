65474af399235cc3f34eabf429c296f217a771f0
Victor Makarov <vitja.makarov@gmail.com>
2023-05-20 13:35:47 +0400

    sway update

--- wayland/sway-idle.sh
+++ wayland/sway-idle.sh
@@ -5,8 +5,8 @@ LOCK_CMD=sway-lock.sh
 exec swayidle "$@" -w \
          lock         $LOCK_CMD  \
          before-sleep $LOCK_CMD  \
-         timeout 5    'pgrep swaylock -U $UID && sway-dpms off' \
-         resume       'pgrep swaylock -U $UID && sway-dpms on'  \
+         timeout 5    "pgrep swaylock -U $UID && sway-dpms off" \
+         resume       "pgrep swaylock -U $UID && sway-dpms on"  \
          timeout 120  $LOCK_CMD  \
-         timeout 125  'sway-dpms off' \
-         resume       'sway-dpms on'  \
+         timeout 125  "sway-dpms off" \
+         resume       "sway-dpms on"  \
