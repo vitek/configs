d7b4e071775acceda34c6286590f95ccfd1816ed
Victor Makarov <vitja@yandex-team.ru>
2023-05-24 17:43:51 +0400

    fix sway lock

--- wayland/sway-lock.sh
+++ wayland/sway-lock.sh
@@ -5,7 +5,7 @@ SWAYLOCK_ARGS="-f -c 000000"
 BG=$(desktopctl  bg-image 2> /dev/null)
 
 if [ "x$BG" != "x" ]; then
-    SWAYLOCK_ARGS="-i $BG"
+    SWAYLOCK_ARGS="$SWAYLOCK_ARGS -i $BG"
 fi
 
 swaymsg 'input "*" xkb_switch_layout 0'
