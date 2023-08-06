9706b075ff6207c6894d2b63a66237859d0a73ab
Victor Makarov <vitja.makarov@gmail.com>
2023-08-06 12:55:30 +0400

    emacs: add basic vertico config

---
+++ emacs/site-lisp/vitja-vertico.el
@@ -0,0 +1,42 @@
+;;; vitja-vertico.el --- Emacs configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Enable vertico
+(use-package vertico
+  :custom
+  (vertico-count 13)                    ; Number of candidates to display
+  (vertico-resize t)
+  (vertico-cycle nil) ; Go from last to first candidate and first to last (cycle)?
+
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+(use-package marginalia
+  :custom
+  (marginalia-max-relative-age 0)
+  (marginalia-align 'right)
+  :init
+  (marginalia-mode))
+
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
+(provide 'vitja-vertico)
+;;; vitja-vertico.el ends here
