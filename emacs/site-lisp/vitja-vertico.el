a1b3eabc6099a5fea8e56a8a0c934e4449e42b42
Victor Makarov <vitja@yandex-team.ru>
2023-08-08 19:26:58 +0400

    emacs: update vertico setup

--- emacs/site-lisp/vitja-vertico.el
+++ emacs/site-lisp/vitja-vertico.el
@@ -9,6 +9,9 @@
   (vertico-resize t)
   (vertico-cycle nil) ; Go from last to first candidate and first to last (cycle)?
 
+  :hook
+  (minibuffer-setup . vertico-repeat-save) ; Make sure vertico state is saved
+
   :init
   (vertico-mode)
 
@@ -25,6 +28,12 @@
   ;; (setq vertico-cycle t)
   )
 
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
 (use-package marginalia
   :custom
   (marginalia-max-relative-age 0)
@@ -32,11 +41,36 @@
   :init
   (marginalia-mode))
 
-(use-package all-the-icons-completion
-  :after (marginalia all-the-icons)
-  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
   :init
-  (all-the-icons-completion-mode))
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 (provide 'vitja-vertico)
 ;;; vitja-vertico.el ends here
