c79da644ecf6ea8cefefc69981bbd0ae7d8135d6
Victor Makarov <vitja@yandex-team.ru>
2024-02-08 17:29:13 +0300

    fix

--- emacs/site-lisp/my.el
+++ emacs/site-lisp/my.el
@@ -16,5 +16,24 @@
       (call-interactively 'counsel-rg))
      (t (call-interactively 'grep)))))
 
+;; https://stackoverflow.com/questions/5194294/how-to-remove-all-newlines-from-selected-region-in-emacs
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+
+(defun unfill-paragraph-copy (&optional region)
+  (interactive)
+  (message region)
+  )
+
+
+;; handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 (provide 'my)
 ;;; my.el ends here
