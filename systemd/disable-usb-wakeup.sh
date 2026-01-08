9b4455049e40bff3e2e4b3948677ada6ef7ef056
Victor Makarov <vitja.makarov@gmail.com>
2026-01-08 18:49:28 +0300

    fix

--- systemd/disable-usb-wakeup.sh
+++ systemd/disable-usb-wakeup.sh
@@ -4,9 +4,8 @@ DEVICES="XHCI"
 ACPI_WAKEUP=/proc/acpi/wakeup
 
 for dev in $DEVICES; do
-    echo $dev
     if cat $ACPI_WAKEUP |grep -q "^$dev	.*enabled"; then
-        echo "Disabling wakup for $dev"
+        echo "Disabling wake-up for $dev"
         echo "$dev" > $ACPI_WAKEUP
     fi
 done
